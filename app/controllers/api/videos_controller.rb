ddb8b9fd5ab2259113484957191f74da9d73cac0
github username <github email address>
2020-10-18 12:51:53 -0400

    added crud

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,8 +1,30 @@
 class API::VideosController < ApplicationController 
-
+   
     def index
-        videos = Video.all 
-        render json: videos, status: 200
+        @videos = Video.all 
+        render json: @videos, status: 200
     end 
 
+    def show
+        @video = Video.find(params[:id])
+        render json: @video, status: 200
+    end 
+
+    def create
+        @video = Video.new(video_params)
+        if @video.save
+        render json: @video, status: 201
+        else 
+            render json: {
+                errors: {
+                    messages: @video.errors.messages 
+        } 
+            }, status: 422
+        end 
+    end 
+
+    private 
+    def video_params 
+        params.require(:video).permit(:title, :description, :youtube_video_id)
+    end 
 end 
